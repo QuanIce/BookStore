80801f90e58f4e6276b1fad1a8ccb79f5373df2b
quando <quand@QUAN>
2023-02-21 15:01:10 +0700

    Quan Finish

---
+++ BookStore/Controllers/ClientController.cs
@@ -0,0 +1,49 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
+
+namespace BookStore.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("/")]
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Product.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var books = _context.Product.Find(id);
+            return View(books);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Search(string keyword)
+        {
+            var books = _context.Product.Where(x=>x.Title.Contains(keyword)).ToList();
+            if (!String.IsNullOrEmpty(keyword))
+            {
+                TempData["Message"] = "No book found";
+            }
+            return View("Index", books);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BookStore/Data/Migrations/20230220175712_IntitalCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Data.Migrations
+{
+    public partial class IntitalCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
